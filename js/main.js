ea0f473789c2f6aa50e06265ea885171b6209748
Islom Toshpolatov <nanotexnolagiya@gmail.com>
2023-08-24 23:46:53 +0500

    refactor: added watcher for prefers-color-scheme, used variable instead of string in order to avoid miss spelling.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
-// On the page load set the localStorage theme value to existing one if there is one. Otherwise, set it to 'light'
-localStorage.setItem('theme', localStorage.getItem('theme') || 'light');
+const THEME_KEY = 'theme'
 
-// Change theme to dark if the localStorage has that setting on
-if (localStorage.getItem('theme') === 'dark') {
-  document.body.classList.add('page__body--dark');
-}
+document.addEventListener('DOMContentLoaded', () => {
+  // On the page load set the localStorage theme value to existing one if there is one. Otherwise, set it to 'light'
+  localStorage.setItem(THEME_KEY, localStorage.getItem(THEME_KEY) || 'light');
+
+  // Change theme to dark if the localStorage has that setting on
+  if (localStorage.getItem(THEME_KEY) === 'dark') {
+    document.body.classList.add('page__body--dark');
+  }
+})
 
 const elThemeToggleButton = document.querySelector('.theme-toggle-button');
 
@@ -15,9 +19,16 @@ if (elThemeToggleButton) {
 
     // Update localStorage theme value
     if (document.body.classList.contains('page__body--dark')) {
-      localStorage.setItem('theme', 'dark');
+      localStorage.setItem(THEME_KEY, 'dark');
     } else {
-      localStorage.setItem('theme', 'light');
+      localStorage.setItem(THEME_KEY, 'light');
     }
   });
 }
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+  if (e.matches) {
+    document.body.classList.add('page__body--dark');
+    localStorage.setItem(THEME_KEY, 'dark');
+  }
+});
