5c4fc9b12a80d56745b222ac81953b480d614603
muhammadjewel <msuratov@gmail.com>
2023-08-24 22:51:22 +0500

    Save page theme to localStorage

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
+// On the page load set the localStorage theme value to existing one if there is one. Otherwise, set it to 'light'
+localStorage.setItem('theme', localStorage.getItem('theme') || 'light');
+
+// Change theme to dark if the localStorage has that setting on
+if (localStorage.getItem('theme') === 'dark') {
+  document.body.classList.add('page__body--dark');
+}
+
 const elThemeToggleButton = document.querySelector('.theme-toggle-button');
 
 if (elThemeToggleButton) {
   elThemeToggleButton.addEventListener('click', function () {
+    // Toggle page theme
     document.body.classList.toggle('page__body--dark');
+
+    // Update localStorage theme value
+    if (document.body.classList.contains('page__body--dark')) {
+      localStorage.setItem('theme', 'dark');
+    } else {
+      localStorage.setItem('theme', 'light');
+    }
   });
 }
