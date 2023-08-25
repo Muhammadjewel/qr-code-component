a120a223df6c14f2a04e260992bfb779d4f88070
muhammadjewel <msuratov@gmail.com>
2023-08-25 21:25:43 +0500

    Refactor theme switching into a function

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 const elThemeTogglerButton = document.querySelector('.theme-toggle-button');
 
+function switchTheme () {
+  const elRoot = document.documentElement
+  let dataTheme = elRoot.getAttribute('data-theme');
+
+  let newTheme = (dataTheme === 'light') ? 'dark' : 'light';
+
+  elRoot.setAttribute('data-theme', newTheme);
+
+  localStorage.setItem('theme', newTheme);
+}
+
 if (elThemeTogglerButton) {
-  elThemeTogglerButton.addEventListener('click', function () {
-    const elRoot = document.documentElement
-    let dataTheme = elRoot.getAttribute('data-theme');
-
-    let newTheme = (dataTheme === 'light') ? 'dark' : 'light';
-
-    elRoot.setAttribute('data-theme', newTheme);
-
-    localStorage.setItem('theme', newTheme);
-  });
+  elThemeTogglerButton.addEventListener('click', switchTheme);
 }
