6bac2fa9678ff0729cc3ba47a6f8016b4250d67b
muhammadjewel <msuratov@gmail.com>
2023-08-25 21:11:00 +0500

    Add theme toggler function with localStorage saving and adapting to device's theme

--- js/main.js
+++ js/main.js
@@ -1,23 +1,14 @@
-// On the page load set the localStorage theme value to existing one if there is one. Otherwise, set it to 'light'
-localStorage.setItem('theme', localStorage.getItem('theme') || 'light');
+const elThemeTogglerButton = document.querySelector('.theme-toggle-button');
 
-// Change theme to dark if the localStorage has that setting on
-if (localStorage.getItem('theme') === 'dark') {
-  document.body.classList.add('page__body--dark');
-}
+if (elThemeTogglerButton) {
+  elThemeTogglerButton.addEventListener('click', function () {
+    const elRoot = document.documentElement
+    let dataTheme = elRoot.getAttribute('data-theme');
 
-const elThemeToggleButton = document.querySelector('.theme-toggle-button');
+    let newTheme = (dataTheme === 'light') ? 'dark' : 'light';
 
-if (elThemeToggleButton) {
-  elThemeToggleButton.addEventListener('click', function () {
-    // Toggle page theme
-    document.body.classList.toggle('page__body--dark');
+    elRoot.setAttribute('data-theme', newTheme);
 
-    // Update localStorage theme value
-    if (document.body.classList.contains('page__body--dark')) {
-      localStorage.setItem('theme', 'dark');
-    } else {
-      localStorage.setItem('theme', 'light');
-    }
+    localStorage.setItem('theme', newTheme);
   });
 }
